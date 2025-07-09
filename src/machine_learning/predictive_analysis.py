72ddbedef8a05a1ae9c4c5f2578d16df68b39247
MJ <maarten_ridder@hotmail.com>
2025-07-09 10:33:01 +0000

    reset target_map again to test impact on render

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -56,7 +56,7 @@ def load_model_and_predict(my_image, version):
     pred_proba = model.predict(my_image)[0, 0]
 
     # Rephrased 'powdery_mildew' to 'Infected' for clarity and ease of use.
-    target_map = {v: k for k, v in {'Healthy': 0, 'Powdery_mildew': 1}.items()}
+    target_map = {v: k for k, v in {'Healthy': 0, 'Infected': 1}.items()}
     pred_class = target_map[pred_proba > 0.5]
     if pred_class == target_map[0]:
         pred_proba = 1 - pred_proba
